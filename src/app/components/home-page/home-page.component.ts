77494919e67fd26e79d10ad5390ac21918249925
VT71 <56217276+VT71@users.noreply.github.com>
2024-07-30 22:57:38 +0300

    Added projects section + project image

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroSectionComponent } from '../intro-section/intro-section.component';
+import { ProjectsSectionComponent } from "../../projects-section/projects-section.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IntroSectionComponent],
+  imports: [IntroSectionComponent, ProjectsSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
