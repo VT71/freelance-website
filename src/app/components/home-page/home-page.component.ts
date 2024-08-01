718050530863102a32840317da4b172422afd6b2
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-01 18:10:28 +0300

    Moved components to a separate folder

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IntroSectionComponent } from '../intro-section/intro-section.component';
-import { ProjectsSectionComponent } from "../../projects-section/projects-section.component";
-import { TestimonialsSectionComponent } from '../../testimonials-section/testimonials-section.component';
+import { ProjectsSectionComponent } from "../projects-section/projects-section.component";
+import { TestimonialsSectionComponent } from '../testimonials-section/testimonials-section.component';
 
 @Component({
   selector: 'app-home-page',
