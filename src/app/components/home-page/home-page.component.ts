4e72fe2b1e9605cbef3f52459dd549b669780358
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-01 19:22:03 +0300

    Added cost estimator selections

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { IntroSectionComponent } from '../intro-section/intro-section.component';
 import { ProjectsSectionComponent } from "../projects-section/projects-section.component";
 import { TestimonialsSectionComponent } from '../testimonials-section/testimonials-section.component';
+import { QuoteComponent } from "../quote/quote.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IntroSectionComponent, ProjectsSectionComponent, TestimonialsSectionComponent],
+  imports: [IntroSectionComponent, ProjectsSectionComponent, TestimonialsSectionComponent, QuoteComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './quote.component.html',
-  styleUrl: './quote.component.css'
+  styleUrl: './quote.component.css',
 })
 export class QuoteComponent {
+  public numberOfPages: number = 0;
+  public designType: number = 0;
+  public type: number = 0;
 
+  public selectionHandle(value: number, type: string) {
+    if (type === 'number-of-pages') {
+      this.numberOfPages = value;
+    } else if (type === 'design') {
+      this.designType = value;
+    } else if (type === 'type') {
+      this.type = value;
+    }
+  }
 }
