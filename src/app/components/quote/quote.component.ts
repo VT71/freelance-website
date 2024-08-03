08a23075bb3d66ac9369b51166a1c15a6e433bd5
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-03 14:23:40 +0300

    Added cost estimator logic

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -8,11 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './quote.component.css',
 })
 export class QuoteComponent {
-  public numberOfPages: number = 0;
-  public designType: number = 0;
-  public type: number = 0;
+  public numberOfPages: string = '';
+  public designType: string = '';
+  public type: string = '';
 
-  public selectionHandle(value: number, type: string) {
+  public duration: number = 0;
+  public totalCost: number = 0;
+
+  public selectionHandle(value: string, type: string) {
     if (type === 'number-of-pages') {
       this.numberOfPages = value;
     } else if (type === 'design') {
@@ -20,5 +23,55 @@ export class QuoteComponent {
     } else if (type === 'type') {
       this.type = value;
     }
+
+    if (this.numberOfPages !== '' && this.designType !== '' && this.type !== '') {
+      this.generateCostTime();
+    }
+  }
+
+  public generateCostTime(): void {
+    let cost = 0;
+    let time = 0;
+
+    switch (this.numberOfPages) {
+      case '1':
+        cost += 150;
+        time += 2;
+        break;
+      case '2-3':
+        cost += 300;
+        time += 5;
+        break;
+      case '4+':
+        cost += 500;
+        time += 8;
+        break;
+    }
+
+    switch (this.designType) {
+      case 'Basic':
+        cost *= 1.5;
+        time *= 1.5;
+        break;
+      case 'Intermediate':
+        cost *= 1.8;
+        time *= 1.8;
+        break;
+      case 'Advanced':
+        cost *= 2.2;
+        time *= 2.2;
+        break;
+    }
+
+    switch (this.type) {
+      case 'E-Shop':
+      case 'Bookings':
+        cost *= 1.8;
+        time *= 1.8;
+        break;
+    }
+
+    this.totalCost = cost;
+    this.duration = time;
   }
 }
