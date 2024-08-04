e1a4057fb1435b00af0cb1e15f877ac3748eed99
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-04 23:32:36 +0300

    Added footer section for the website

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,11 +3,12 @@ import { IntroSectionComponent } from '../intro-section/intro-section.component'
 import { ProjectsSectionComponent } from "../projects-section/projects-section.component";
 import { TestimonialsSectionComponent } from '../testimonials-section/testimonials-section.component';
 import { QuoteComponent } from "../quote/quote.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IntroSectionComponent, ProjectsSectionComponent, TestimonialsSectionComponent, QuoteComponent],
+  imports: [IntroSectionComponent, ProjectsSectionComponent, TestimonialsSectionComponent, QuoteComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
