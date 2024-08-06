aacc2189cb19d41bdb3617dec1994354d8a11696
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-06 21:50:45 +0300

    Responsiveness changes for the intro section

--- src/app/components/intro-section/intro-section.component.ts
+++ src/app/components/intro-section/intro-section.component.ts
@@ -1,17 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  Breakpoints,
+  BreakpointObserver,
+  BreakpointState,
+} from '@angular/cdk/layout';
+import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-intro-section',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, AsyncPipe],
   templateUrl: './intro-section.component.html',
   styleUrl: './intro-section.component.css',
 })
-export class IntroSectionComponent {
+export class IntroSectionComponent implements OnInit {
   public activeNavLink: string = 'intro';
+  public breakPointPromise$!: Observable<boolean>;
+
+  public mobileNavOpen: boolean = false;
+  public hideMobileMenu: boolean = true;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakPointPromise$ = this.responsive
+      .observe([Breakpoints.XSmall])
+      .pipe(
+        map((result) => {
+          //   const breakpoints = result.breakpoints;
+          //   console.log(JSON.stringify(breakpoints));
+          //   console.log('Handset props: ' + JSON.stringify(Breakpoints.Handset));
+          //   console.log('Tablet props: ' + JSON.stringify(Breakpoints.Tablet));
+          //   if (breakpoints[Breakpoints.Handset]) {
+          //     console.log('Handset');
+          //   }
+          //   if (breakpoints[Breakpoints.Tablet]) {
+          //     console.log('Tablet');
+          //   }
+          if (result.matches) {
+            console.log('MATCHES');
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+  }
 
   public onClickNav(label: string): void {
     this.activeNavLink = label;
+  }
+
+  public handleMobileNavDisplay(): void {
+    this.mobileNavOpen = !this.mobileNavOpen;
   }
 }
